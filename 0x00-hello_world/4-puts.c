a94837c732efa23989be1e3e10d3daec75d23cc1
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-18 10:13:20 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle")
+#include<stdio.h>
+int main(voids){puts("Programming is like building a multilingual puzzle"); return 0;}
