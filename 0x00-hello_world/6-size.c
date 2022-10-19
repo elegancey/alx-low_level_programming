084910d0251714a50f476062d786998c371c22ea
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-19 06:58:13 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,17 @@ int main(void)
  * Return: Always 0
  */
 {
+	
 	printf("Size of a char: %d%s",1," byte(s)\n");
 	printf("Size of an int: %d%s",4," byte(s)\n");
 	printf("Size of a long int: %d%s",4," byte(s)\n");
 	printf("Size of a long long int: %d%s",8," byte(s)\n");
 	printf("Size of a float: %d%s",4," byte(s)\n");
 	return (0);
+}
+
+int main(void)
+{
 
 	printf("Size of a char: %d%s",1," byte(s)\n");
 	printf("Size of an int: %d%s",4," byte(s)\n");
