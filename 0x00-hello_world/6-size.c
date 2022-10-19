64d61c69001e7f164019e24871918dc58d927e77
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-19 06:22:04 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Size of a char: %c%c%c and my age is %d.\n", 1, "byte(s)", "R", "E", 28);
+	printf("Size of a char:%d%s",1,"byte(s)");
 	return (0);
 }
