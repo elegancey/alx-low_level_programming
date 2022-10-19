4d8046cf8f26623520599e5b77133cfcd434c944
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-19 06:44:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,6 @@ int main(void)
 	printf("Size of an int: %d%s",4," byte(s)\n");
 	printf("Size of a long int: %d%s",4," byte(s)\n");
 	printf("Size of a long long int: %d%s",8," byte(s)\n");
-	printf("Size of a float: %d%s",4," byte(s)");
+	printf("Size of a float: %d%s",4," byte(s)\n");
 	return (0);
 }
