f6e9068e8996a679724d62e7fff5c1f2f2af519b
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-22 08:36:40 -0700

    alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,12 @@ int main(void)
 {
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
-		if (letter = 'q'||'e')
+	{
+		if (letter == 'q' || letter == 'e')
 			continue
 		else
 			putchar(letter);
+	}
 	putchar('\n');
 
 	return (0);
