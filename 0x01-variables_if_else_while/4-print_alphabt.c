cc5735e2374f3ac7b8260b0c7575d1ec0bea596f
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-22 07:47:53 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print with exception
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter = 'q') || (letter = 'e')
+			continue
+		else
+			putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
