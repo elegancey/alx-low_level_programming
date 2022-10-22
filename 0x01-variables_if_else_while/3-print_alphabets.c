b99f81595c555dee8b03b9e1d4e79eb3f2b36a0d
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-22 04:21:59 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print lower and uppercase letters
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter
+		for (letter = "a"; letter <= "z"; letter++)
+			putchar(letter);
+		putchar('\n');
+
+		return(0);
+
+		for (letter = "A"; letter <= "Z"; letter++)
+			putchar(letter);
+		putchar('\n');
+
+		return(0);
+}
