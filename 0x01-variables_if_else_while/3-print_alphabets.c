6d40da1fa1bf68db15d8be914991af77110ba2e7
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-22 04:30:50 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,8 +12,10 @@ int main(void)
 		for (letter = "a"; letter <= "z"; letter++)
 			putchar(letter);
 
-		for (letter = "A"; letter <= "Z"; letter++)
-			putchar(letter);
+	char upper
+
+		for (upper = "A"; upper <= "Z"; upper++)
+			putchar(upper);
 		putchar('\n');
 
 		return(0);
