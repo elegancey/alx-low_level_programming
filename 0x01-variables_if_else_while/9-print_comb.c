bdf9e701c9314d0af25a80fcf82936d1dc79f2bd
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-24 04:06:42 -0700

    combine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print combinations
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; numberr++)
+		putchar(number", ");
+	putchar('\n');
+
+	return (0);
+}
