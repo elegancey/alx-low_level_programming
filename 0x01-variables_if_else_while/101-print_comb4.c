e6f2c4546263f2a81ed50f5d7dc6a46c835d501e
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-24 14:09:51 -0700

    combine4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 	int num1, num2, num3;
+
 	for (num1 = 0; num1 <= 8; num1++)
 	{
-		for (num2 = num1 +1; num2 < 9; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
 			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
@@ -28,6 +29,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
