3dd7fa8934cd79af62c1c3b19a92f34720dc6db1
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-24 07:01:57 -0700

    combinte

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 	int number;
 	for (number = 0; number <= 10; number++)
 	{
-		putchar((number % 9) + '0');
+		putchar((number % 10) + '0');
 		if (number == 1 || number == 0)
 			continue;
 
