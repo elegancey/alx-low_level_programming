abd30c01d660abe0adaa0ff74d282f8837dd9c21
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-25 07:50:43 -0700

    print

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,15 @@
 #include <main.h>
 #include <stdio.h>
 /**
- * main - Print 
+ * main - Print putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n = "_putchar";
+
+	putchar(n);
+
+	return (0);
+}
