e2f4a2cd9600fb48fbafcc2824781f1d319df3ab
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-26 09:14:18 -0700

    print 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,14 @@
 
 void print_alphabet_x10(void)
 {
+	int count = 0;
 	char letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	print_alphabet_x10(10);
+
+	while (count++ <=9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
 	_putchar('\n');
 
 }
