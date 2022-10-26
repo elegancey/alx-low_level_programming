497bb7e7eb216a3a32d6fe7fa96f5457e18fb21f
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-26 04:27:57 -0700

    print low

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Print lowercase
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+
+	return (0);
+}
