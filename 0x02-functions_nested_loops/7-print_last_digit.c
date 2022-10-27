06bf3ec24cca6a85d96da4156799874c86f45d3f
Hamzat Abdulawwal <hamzatabdulawwal@example.com>
2022-10-27 06:55:44 -0700

    print last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of a number
+ * @n: The character used
+ *
+ * Return: Last digit
+ */
+int print_last_digit(int n)
+{
+	for (n = 0; n <= 9; n++)
+		return (n[-1]);
+}
