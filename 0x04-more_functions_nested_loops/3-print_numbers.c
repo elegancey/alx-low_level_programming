b6965dafda328b0840be1fd13f9496ea9c140142
Hamzat Abdul-awwal <hamzatabdulawwal@gmail.com>
2023-01-02 01:53:45 -0800

    print number with new lines

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,11 @@
 
 void print_numbers(void)
 {
-	int i = '0';
-
-	for (; i <= '9'; i++)
-		putchar(i);
+	int num;
+	
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+	
 	_putchar('\n');
 
 }
