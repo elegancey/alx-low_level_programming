ff0f7e485c49bab3e64e46ee62c669a84658b933
Hamzat Abdul-awwal <hamzatabdulawwal@gmail.com>
2023-01-03 22:57:05 -0800

    put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Write a function that prints a string, followed
@@ -9,9 +10,15 @@
  */
 
 void _puts(char *str)
+
 {
-	while (*str)
-		_putchar(*str++);
+	int i = 0;
 	
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
+
 }
